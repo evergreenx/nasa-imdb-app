0bb6c4f4c57b85ecc26879654a56f3dc67971681
Ido Evergreen <dicksonevergreenido@gmail.com>
2022-04-29 12:10:32 +0100

    bug: fix loader on mobile screens

--- .history/src/Components/Loader/Index_20220429120912.js
+++ .history/src/Components/Loader/Index_20220429120912.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Index = () => {
   return (
-    <div className="bg-black h-screen w-full text-center px-10 text-white text-5xl flex items-center justify-center">
+    <div className="bg-black h-screen w-full pt-20 px-10 text-white text-5xl flex items-center justify-center">
       <h1 className=" ">Loading data .... Please wait</h1>
     </div>
   );
